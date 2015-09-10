724cf5b65f22e0ea29ce77c30822cff24798dc3c
Lowbrassrage <dellis@nnu.edu>
2015-09-10 16:50:47 -0700

    cleaned node.py and wrote unittest.

--- node.py
+++ node.py
@@ -16,57 +16,17 @@ class Node(object):
     def setRight(self, right):
         self.rightNode = right
 
-    # def __str__(self):
-    #     return str(self.leftNode), str(self.value), str(self.rightNode)
     def printMe(self):
         # if node has no children:
-        #   print value
-        # else:
-        #   if node has left:
-        #       leftNode.printMe()
-        #   if node has right:
-        #       rightNode.printMe()
         if self.leftNode is None and self.rightNode is None:
+            # print value
             print(self.value + ' ', end="")
         else:
+            # if node has left:
             if self.leftNode is not None:
                 self.leftNode.printMe()
+            # print value
             print(self.value + ' ', end="")
+            # if node has right:
             if self.rightNode is not None:
                 self.rightNode.printMe()
-
-
-
-def main():
-    # nodeList = []
-    root = Node('2')
-    node7 = Node('7')
-    node5 = Node('5')
-    node9 = Node('9')
-    node25 = Node('25')
-    node6 = Node('6')
-    node35 = Node('35')
-    node11 = Node('11')
-    node4 = Node('4')
-
-    # nodeList.append(node1)
-    # nodeList.append(node2)
-    # nodeList.append(node3)
-    # nodeList.append(node4)
-
-    root.setLeft(node7)
-    root.setRight(node5)
-    node7.setLeft(node25)
-    node7.setRight(node6)
-    node6.setLeft(node35)
-    node6.setRight(node11)
-    node5.setRight(node9)
-    node9.setLeft(node4)
-
-    # for node in nodeList:
-    #     print(node)
-
-    root.printMe()
-
-if __name__ == '__main__':
-    main()
